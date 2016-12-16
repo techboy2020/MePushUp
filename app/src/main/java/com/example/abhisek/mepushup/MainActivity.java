82c04acf4b522b356730cb3c04c48a52586224f7
Techboy2020 <yohoncho@gmail.com>
2016-12-16 10:16:00 +0530

    Modified Main Activity

---
+++ app/src/main/java/com/example/abhisek/mepushup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.abhisek.mepushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void TestUpload(){
+        int i=1+1;
+        int ii=i+1;
+        int iii=i+ii;
+    }
+}
